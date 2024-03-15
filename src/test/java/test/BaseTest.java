9213d8158c9c896e62ceb47fff8c55d80cbfb09a
Chetana <chetna.noronha@gmail.com>
2024-03-15 14:37:51 +0530

    initial commit

---
+++ src/test/java/test/BaseTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class BaseTest {
+	public static WebDriver driver;
+	String url = "https://fb.com";
+
+	@BeforeTest
+	public void setup() {
+		// open the browser
+		driver = new ChromeDriver(); //open the Chrome browser
+	}
+
+	@AfterTest
+	public void teardown() {
+		// close the browser
+		driver.get(url);
+	}
+}
---
+++ src/test/java/test/BasicValidation.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class BasicValidation extends BaseTest {
+
+	@Test
+		public void urlValidation() {
+		String expectedResult = "https://www.facebook..com/";
+		String actualResult = driver.getCurrentUrl();
+		
+		//display the results
+		Reporter.log("expectedResult = " + expectedResult);
+		Reporter.log("actual result =" + actualResult);
+		
+		//comparison
+		assertTrue(actualResult.equals(expectedResult), "Mismatch in the url");
+		
+	}
+}
